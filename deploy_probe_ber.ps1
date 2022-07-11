a419a88f920090b2bc6718369f8acc095afa6e1a
fsueirogomendio <fsueirogomendio@e908984c-4dde-4560-9d01-df6527ee11f5>
2022-07-11 08:07:57 +0000

    fix deploy_probe_ber to allo $ignorevault and uses key file
    git-svn-id: https://por-reposerver.por-ngcs.lan/svn/install/trunk/ngcs_deploy/probe-deployment@2502 e908984c-4dde-4560-9d01-df6527ee11f5

--- deploy_probe_ber.ps1
+++ deploy_probe_ber.ps1
@@ -14,6 +14,9 @@ privnet: vlan for the private network interface
 mac: this parameter will be automatically feeded during the script just after the VM creation
 dhcpfqdn: specifiy the fqdn that will relies on the public IP so during the script, the DHCP reervation will be automatically created.
 
+.PARAMETER ignorevault
+If you set it to true, it will not use VAULT and it ask you for all credentials
+Default value: $false
 .PARAMETER probeFile
 Mandatory
 You must specify the path to your CSV file, see description for more details about it.
@@ -36,7 +39,7 @@ Online version: https://confluence.united-internet.org/display/~jlobatoalonso/de
 
 Param(
     [Parameter(Mandatory = $True)] [string]$probeFile = "",
-    [Parameter(Mandatory = $True)] [string]$priv_key = "",
+	[Boolean]$ignorevault = $false,
     [Boolean]$force = $false,
     [Boolean]$createdns = $true
     )
@@ -88,17 +91,39 @@ try {
     exit
 }
 
-# Default variables
-$DomainProvisioningUser="DomainProvisioning@por-ngcs.lan"
-$DomainProvisioningPass="KI/%43e42ku7t412MHJG2..71fEWS(fd"
+#Ask for credentials and connect to vCenter
+$myCredentials = Get-Credential -WarningAction:SilentlyContinue -Message "Please provide credentials from @ionos.com to connect to vcenter" -username "@ionos.com"
+
+if (!$ignorevault){
+	# Default variables
+	$DomainProvisioningUser="DomainProvisioning@por-ngcs.lan"
+	$DomainProvisioningPass = VAULT-GetSecret -uri $vaulturi -engine $vaultengine -secretpath $vaultpath -credentials $myCredentials -secretkey DomainProvisioning
+	
+	#create pssuser key
+	$pssuser_key = VAULT-GetSecret -uri $vaulturi -engine $vaultengine -secretpath $vaultpath -credentials $myCredentials -secretkey pssuser_key
+}else{
+	# Default variables
+	$DomainProvisioningUser="DomainProvisioning@por-ngcs.lan"
+	$DomainProvisioningPass = Read-Host ("Please, introduce DomainProvisioning@por-ngcs.lan password")
+	
+	#create pssuser key
+	$pssuser_key = get-content .\pssuser.key
+}
+
+[IO.File]::WriteAllLines("$($pwd.path)\tmp_key", $pssuser_key)
+$ACL=Get-Acl .\tmp_key
+$ACL.SetAccessRuleProtection($true,$false)
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$(whoami)","write,read,modify","Allow")
+$ACL.SetAccessRule($AccessRule)
+$ACL | Set-Acl -Path tmp_key
+
+$priv_key = "$($pwd.path)\tmp_key"
+
 $location = "network"
 $template = "co7_64_puppet5"
 $customization = "por-generic"
 $numcpu = 1
 $ram = 2
-
-#Ask for credentials and connect to vCenter
-$myCredentials = Get-Credential -WarningAction:SilentlyContinue -Message "Please provide credentials from @ionos.com to connect to vcenter" -username "@ionos.com"
 
 foreach ($probe in $probeList)
 {
